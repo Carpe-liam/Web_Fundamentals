a9efbfcbd9b99edee6b72603713811d8e3f2a931
Jim Reeder <reederje@gmail.com>
2021-08-18 11:22:10 -0700

    W1D3 Morning Code

---
+++ 1. Algos/W1D3.js
@@ -0,0 +1,19 @@
+//Boolean
+var isTired = true;
+var isCaffeinated = true;
+
+// keyword | logical check | code block
+if( 2 == 2 ){
+    console.log("that is true!!")
+}
+if( 2 == 3 ){
+    console.log("that is true!!")
+}
+
+if(isTired){
+    console.log("get coffee")
+}
+
+if(0){
+    console.log("is this true???")
+}
